24ecd0f6393d803a3ace00809fa088d5f3ef0486
Brent Hansen <mr.brent.hansen@gmail.com>
2023-09-18 15:32:37 -0500

    Timestamps

--- postgres_tables.sql
+++ postgres_tables.sql
@@ -1,42 +1,28 @@
-
-CREATE TABLE users (
+CREATE TABLE if not exists users (
 	user_id uuid PRIMARY key not null,
 	username varchar (18) UNIQUE NOT null,
 	username_distinct varchar (18) unique not null,
-	user_create_ts timestamp default CURRENT_TIMESTAMP
+	user_create_ts timestamptz default current_timestamp
 );
 
-create table friends (
+create index if not exists username_search on users(username_distinct);
+
+
+create table if not exists friends (
 	user_id uuid primary key not null,
 	friend_id uuid not null,
-	friend_create_ts timestamp default current_timestamp,
+	friend_create_ts timestamptz default current_timestamp,
 
 	foreign key (user_id) references users(user_id),
 	foreign key (friend_id) references users(user_id)
 );
 
-create table friend_requests (
+create table if not exists friend_requests (
 	user_id uuid primary key not null,
 	friend_id uuid not null,
-	friend_create_ts timestamp default current_timestamp,
+	friend_request_ts timestamptz default current_timestamp,
 	
 	foreign key (user_id) references users(user_id),
 	foreign key (friend_id) references users(user_id)
 );
 
-
-create index username_search on users(username_distinct);
-
-alter database postgres set timezone to 'UTC';
-SELECT pg_reload_conf();
-
-insert into users(user_id, username, username_distinct) 
-values ('0461309d-d78b-4d88-b57d-7838842b8998', 'Admincrystal', 'admiuncrystal');
-
-SELECT * FROM users u;
-
-select * from friends f;
-
-drop table users;
-
-insert into 
