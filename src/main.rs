9fc5c7157ddcdb99de1adc6bdba0caff2c0c4028
Brent Hansen <mr.brent.hansen@gmail.com>
2023-09-18 11:25:57 -0500

    Configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate macro_rules_attribute;
+
 mod structs;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
@@ -5,40 +8,21 @@ use sqlx::{Pool, Postgres};
 use crate::structs::User;
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
 
-use dotenv::dotenv;
 use std::env;
 use config::Config;
-use std::collections::HashMap;
-use lazy_static::lazy_static;
-use serde_json::Value;
-use std::sync::RwLock;
 
-lazy_static! {
-    static ref SETTINGS: RwLock<Config> = RwLock::new(Config::builder()
-        .add_source(config::File::with_name("C:/Users/mrbre/CLionProjects/leetcode/tokio_test/src/application-dev.json"))
-        .build()
-        .expect("Couldn't grab config files"));
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
 
-    let username: String = SETTINGS.read().unwrap().get("database_username").unwrap();
-    // let username = env::var("DB_USERNAME").expect("Db username is missing");
-    let conn = PgConnectOptions::new()
-        .host("localhost")
-        .port(5433)
-        .username(&username)
-        .password("postgres");
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect_with(conn)
-        .await.expect("bad connection details");
+    let config = get_config();
+
+    let pool = create_postgres_pool(&config).await;
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(config.clone()))
             .service(health)
             .service(echo)
             .service(json)
@@ -50,6 +34,37 @@ async fn main() -> std::io::Result<()> {
 
 }
 
+async fn create_postgres_pool(configs: &Config) -> Pool<Postgres> {
+    let username: String = configs.get("postgres_username").unwrap();
+    let password: String = configs.get("postgres_password").unwrap();
+    let port: u16 = configs.get("postgres_port").unwrap();
+    let host: String = configs.get("postgres_host").unwrap();
+
+    let conn = PgConnectOptions::new()
+        .host(&host)
+        .port(port)
+        .username(&username)
+        .password(&password);
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect_with(conn)
+        .await.unwrap();
+
+    return pool;
+}
+
+fn get_config() -> Config {
+    let env = &env::var("env").unwrap();
+    let secrets_path = &env::var("secrets.folder.path").unwrap();
+
+
+    let configs = Config::builder()
+        .add_source(config::File::with_name(&format!("{}/postgres_{}.json", secrets_path, env)))
+        .build().unwrap();
+
+    return configs;
+}
+
 
 #[get("/health")]
 async fn health() -> impl Responder {
@@ -58,14 +73,14 @@ async fn health() -> impl Responder {
 
 
 #[post("/echo")]
-async fn echo(req_body: String, pool: web::Data<Pool<Postgres>>,) -> impl Responder {
+async fn echo(req_body: String, pool: web::Data<Pool<Postgres>>, configs: web::Data<Config>) -> impl Responder {
     let values: Vec<User> = sqlx::query_as("SELECT * FROM USERS")
         .fetch_all(&*pool.into_inner())
         .await.unwrap();
 
     println!("{:?}", values);
 
-    HttpResponse::Ok().body(req_body)
+    return HttpResponse::Ok().body(req_body)
 }
 
 
@@ -73,14 +88,12 @@ async fn echo(req_body: String, pool: web::Data<Pool<Postgres>>,) -> impl Respon
 async fn json(pool: web::Data<Pool<Postgres>>, user: web::Json<User>) -> impl Responder {
     // into_inner returns all of the objects data
     let x = user.into_inner();
-    let y = 5;
 
     let values: Vec<User> = sqlx::query_as("SELECT * FROM USERS")
         .fetch_all(&*pool.into_inner())
         .await.unwrap();
 
     println!("{:?}", values);
-    let x = 5;
 
     HttpResponse::Ok().json(x)
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,7 +1,40 @@
+use std::fmt::{Debug};
+// use macro_rules_attribute::derive_alias;
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow};
 
-#[derive(Serialize, Deserialize, FromRow, Debug)]
+// #[derive(Debug)]
+// struct MyError {
+//     err: anyhow::Error,
+// }
+//
+// impl Display for MyError {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         todo!()
+//     }
+// }
+//
+// impl actix_web::error::ResponseError for MyError {
+//
+// }
+// impl From<anyhow::Error> for MyError {
+//     fn from(err: anyhow::Error) -> MyError {
+//         MyError { err }
+//     }
+// }
+
+// impl From<std::io::Error> for anyhow::Error {
+//     fn from(err: anyhow::Error) -> MyError {
+//         MyError { err }
+//     }
+// }
+
+// derive_alias! {
+//     #[derive(Data!)] = #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize)];
+//     #[derive(SqlData!)] = #[derive(Data!, FromRow)];
+// }
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize, FromRow)]
 pub struct User {
     // Required values
     pub username: String,
